1a37ca168a10e5e1dc3baaaa7b9aec4ad6135da4
kael74 <kael74@Geheim>
2016-06-09 01:09:03 -0500

    php Proyecto v0.31

---
+++ index.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link href="css/bootstrap.css" rel="stylesheet" type="text/css"/>
+        <link href="css/bootstrap-theme.css" rel="stylesheet" type="text/css"/>
+        <link href="css/bootstrap-theme.min.css" rel="stylesheet" type="text/css"/>
+        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <link href="css/mouse.css" rel="stylesheet" type="text/css"/>
+        <link href="css/style.css" rel="stylesheet" type="text/css"/>
+        <title>Daimon Words</title>        
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+        <nav class="navbar" id="st-black">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle navbar-inverse" data-toggle="collapse" data-target="#navbar-collapse">
+                        <span class="icon-"></span>
+                    </button>
+
+                    <a href="" class="navbar-brand textA">Daimon Words</a>
+                </div>
+                <div class="collapse navbar-collapse" id="navbar-collapse">
+                    <ul class="nav navbar-nav">
+                        <li><a href="" class="textB">Categorias</a>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        
+        <div class="container  bor">
+            <section>
+                <div class="carousel slide" id="screenshot-carousel" data-ride="carousel">
+                    <div class="carousel-inner">
+                        <div class="item active">
+                            <img src="imagenes/libro1.jpg" alt="Text of the image">
+                        </div>
+                        <div class="item">
+                            <img src="imagenes/libro2.jpeg" alt="Text of the image">
+                        </div>
+                        <div class="item">
+                            <img src="imagenes/libro3.jpg" alt="Text of the image">
+                        </div>
+                        <div class="item">
+                            <img src="imagenes/libro4.jpg" alt="Text of the image">
+                        </div>
+                    </div>
+                    <a href="#screenshot-carousel" class="left carousel-control" data-slide="prev">
+                        <span class="glyphicon glyphicon-chevron-left"></span>
+                    </a>
+                    <a href="#screenshot-carousel" class="right carousel-control" data-slide="next">
+                        <span class="glyphicon glyphicon-chevron-right"></span>
+                    </a>
+                </div>
+            </section>
+            <hr>
+            <div class="limo">
+            <section class="col-md-8" id="left">          
+                <div class="general">
+                    <div class="wild">
+                        <img src="imagenes/libro6.jpg" class="l6">
+                    </div>
+                    <div class="wild">
+                        <h1><span class="icon-star-full"></span>Esa Wea~ :v </h1>
+                    </div>
+                </div>
+                <br>
+                <div class="general">
+                    <div class="wild">
+                        <img src="imagenes/libro7.jpg" class="l6">
+                    </div>                    
+                    <div class="wild">
+                        <h1><span class="icon-star-full"></span><span class="icon-star-full"></span><span class="icon-star-half"></span><span class="icon-star-empty"></span><span class="icon-star-empty"></span></h1>
+                        <h1>The Lord of the Rings</h1>
+                    </div>
+                </div>
+                <br>
+                <div class="general">
+                    <div class="wild">
+                        <img src="imagenes/libro7.jpg" class="l6">
+                    </div>
+                    <div class="wild">
+                        <h1><span class="icon-star-full"></span><span class="icon-star-full"></span><span class="icon-star-half"></span><span class="icon-star-empty"></span><span class="icon-star-empty"></span></h1>
+                        <h1>The Lord of the Rings</h1>
+                    </div>
+                </div>
+            </section>
+            <aside class="col-md-4" id="right">
+                <div>
+                    <h1>miau khe?</h1>
+                    <h1>miau khe?</h1>
+                    <h1>miau khe?</h1>
+                    <h1>miau khe?</h1>
+                    <h1>miau khe?</h1>
+                </div>                
+            </aside>
+            </div>
+        </div>
+            <footer>
+                
+            </footer>        
+        <script src="js/jquery-1.12.4.min.js" type="text/javascript"></script>
+        <script src="js/jquery-2.2.4.min.js" type="text/javascript"></script>
+        <script src="js/bootstrap.min.js" type="text/javascript"></script>
+    </body>
+</html>
